4b601d65fc62d89bc066d30c558f002177c0b104
Isarel Getu <israelgetu101@gmail.com>
2022-02-16 15:55:06 -0600

    Problem Solved 2

--- arrayMethods.js
+++ arrayMethods.js
@@ -45,9 +45,13 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+const postTaxPrices = prices.map(function(element){
+  return element * 1.07
+})  // = prices.map(/* Provide Your Callback Here );
 
+const postTaxPrices = prices.map(elem => elem * 1.07)
 
+console.log(postTaxPrices)
 
 ////////// PROBLEM 3 //////////
 
